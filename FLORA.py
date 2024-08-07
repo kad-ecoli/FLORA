9f4db21fdc97553ba0f1ca5a48c3f5e294163302
Chengxin Zhang <zcx@lh-login1.arc-ts.umich.edu>
2024-08-07 17:03:05 -0400

    check sequence consistent

--- FLORA.py
+++ FLORA.py
@@ -21,6 +21,30 @@ def System(cmd):
     os.system(cmd)
     return
 
+def check_consistent(inputFasta,inputPdb):
+    sequence=''
+    fp=open(inputFasta,'r')
+    for line in fp.read().splitlines():
+        if not line.startswith('>'):
+            sequence+=line
+    fp.close()
+    sequence=sequence.lower()
+    L=len(sequence)
+
+    fp=open(inputPdb,'r')
+    for line in fp.read().splitlines():
+        if not line.startswith('ATOM  '):
+            continue
+        resSeq=int(line[22:26])
+        nt    =line[19].lower()
+        if resSeq<=0 or resSeq>L:
+            print("ERROR! resSeq=%d out of range"%(resSeq))
+            exit()
+        elif sequence[resSeq-1]!=nt:
+            print("ERROR! %s%d != %s"%(sequence[resSeq-1],resSeq,line))
+    fp.close()
+    return
+
 if len(sys.argv)!=4:
     sys.stderr.write(docstring)
     exit()
@@ -29,6 +53,7 @@ inputFasta = sys.argv[1]
 inputPdb   = sys.argv[2]
 outputPdb  = sys.argv[3]
 
+check_consistent(inputFasta,inputPdb)
 System("%s/CSSR/CSSR %s %s %s.constraint"%(
         rootdir,inputFasta,inputPdb,outputPdb))
 System("%s/ViennaRNA/src/bin/RNAfold -i %s --noPS --constraint %s.constraint --enforceConstraint |head -2 |tail -1|grep -ohP '^\\S+'> %s.dbn"%(
